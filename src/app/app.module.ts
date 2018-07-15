8927c4f3b1044fbd01d05c4e1ffd852dcae6bcc6
Abiel Brhane <abielberaki1@gmail.com>
2018-07-15 16:23:50 -0600

    change on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MaterialModule } from './material.module';
 import { AppRouters } from './app.routes';
-
+ 
+// import {MdButtonModule, MdCheckboxModule,MdGridListModule,MdInputModule,MdIconModule} from '@angular/material';
 
 
 import { NgModule } from '@angular/core';
@@ -12,19 +13,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    UserComponent
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     MaterialModule,
-    AppRouters
+    AppRouters,
+    NgbModule.forRoot()
 
   ],
   providers: [],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
     { path: '', component: UserComponent },
-    { path: 'products', component: ProductsComponent }
+    { path: 'products', component: ProductsComponent },
+    { path: 'login', component:LoginComponent}
 ];
 
 @NgModule({
