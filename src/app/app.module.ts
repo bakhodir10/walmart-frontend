8e5bc3412825db2370469ed0affbf5ca59bcfa1e
Bakhodir <bakhodir9293@gmail.com>
2018-07-15 21:00:25 -0500

    added interceptor on each request to set header token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -8,13 +8,13 @@ import { AppRouters } from './app.routes';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { EmployeeComponent } from './employee/employee.component';
-import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuardService } from './auth/auth-guard.service';
 import { AuthService } from './auth/auth.service';
 import { RoleGuardService } from './auth/role-guard.service';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './auth/aut.interceptor.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +33,7 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
 
   ],
-  providers: [AuthService, AuthGuardService, RoleGuardService],
+  providers: [AuthInterceptor, AuthService, AuthGuardService, RoleGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
