7bc0d25a89d5e3e97bfaa71c63304a351eddec02
Abiel Brhane <abielberaki1@gmail.com>
2018-07-16 21:56:41 -0600

    product component

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -1,4 +1,8 @@
 export class Product {
   _id: string;
   name: string;
+  price: number;
+  expire_date: Date;
+  quantity: number;
+  
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,13 +3,16 @@ import {ProductService} from './product.service';
 import {Observable} from 'rxjs/Observable';
 import {Product} from './product.model';
 
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products: Observable<Product>;
+  // products: Observable<Product>;
+  pageTitle: string = 'Product List';
+  errorMessage: string;
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
@@ -18,5 +21,38 @@ export class ProductsComponent implements OnInit {
 
   loadProducts(){
     this.productService.getList().subscribe(res => console.log(res));
+  }
+
+  _listFilter: string;
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.perfromFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: Product[];
+  products: Product[];
+
+  onRatingClicked(message:string):void{
+    this.pageTitle = 'Product List: ' + message;
+  }
+
+  perfromFilter(filterBy: string): Product[]{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: Product) =>
+      product.name.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  OnInit(): void {
+    this.productService.getList().subscribe(
+      products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error => this.errorMessage = <any>error
+    );
   }
 }
